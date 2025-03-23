47319defb0ce65f51fd337ebfcd6e06dedc89f85
Kabeer Jaffri <kabeer11000@gmail.com>
2025-03-24 00:07:12 +0500

    uh oh

--- .next/server/chunks/ssr/[root of the server]__d9fd735e._.js
+++ .next/server/chunks/ssr/[root of the server]__d9fd735e._.js
@@ -93,14 +93,6 @@ function Document() {
                 lineNumber: 9,
                 columnNumber: 7
             }, this),
-            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$externals$5d2f$react$2f$jsx$2d$dev$2d$runtime__$5b$external$5d$__$28$react$2f$jsx$2d$dev$2d$runtime$2c$__cjs$29$__["jsxDEV"])("script", {
-                crossOrigin: "anonymous",
-                src: "//unpkg.com/react-scan/dist/auto.global.js"
-            }, void 0, false, {
-                fileName: "[project]/src/pages/_document.tsx",
-                lineNumber: 13,
-                columnNumber: 7
-            }, this),
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$externals$5d2f$react$2f$jsx$2d$dev$2d$runtime__$5b$external$5d$__$28$react$2f$jsx$2d$dev$2d$runtime$2c$__cjs$29$__["jsxDEV"])("body", {
                 children: [
                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$externals$5d2f$react$2f$jsx$2d$dev$2d$runtime__$5b$external$5d$__$28$react$2f$jsx$2d$dev$2d$runtime$2c$__cjs$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$document$2e$js__$5b$ssr$5d$__$28$ecmascript$29$__["Main"], {}, void 0, false, {
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,14 +6,14 @@ export default function Document() {
       <Head />
       <meta data-rh="true" name="theme-color" content="#FFFFFF" />
       <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16584105712"></script>
-      <script>
+      <script> 
         {`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date());  gtag('config', 'AW-16584105712');`}
       </script>
 
-      <script
+      {/* <script
         crossOrigin="anonymous"
         src="//unpkg.com/react-scan/dist/auto.global.js"
-      />
+      /> */}
       {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" /> */}
       {/* <link
