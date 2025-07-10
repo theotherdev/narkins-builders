e9fb8a8a149cd5acf0263d4be1229f2dcbd4723d
imossaidqadri <imossaidquadri@gmail.com>
2025-07-10 07:26:55 +0500

    legal pages added

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import Footer from '@/components/footerooter/footer';
+import Footer from '@/components/footer/footer';
 
 const Privacy = () => {
   return (
